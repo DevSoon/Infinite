9d1666c4744e21e362a7f4f6010fe39ea4af0267
DevSoon <jssosweet@gmail.com>
2023-11-21 17:09:38 +0900

    feat: workflow 수정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-    assetPrefix: isProd ? '/Infinite/' : undefined,
+    assetPrefix: '/Infinite/',
     output: 'export',
 }
 
 module.exports = nextConfig
-
